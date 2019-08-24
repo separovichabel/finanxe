02f979fbedff93d07bfd4408503a427540a7485f
Abel Separovich <aseparovich@newcore.com.br>
2019-08-24 12:33:47 -0300

    initial commit

---
+++ src/domain/domain.ts
@@ -0,0 +1,15 @@
+export type Transaction = {
+    id: number;
+    value: number;
+    dateTime: Date; 
+};
+
+export type Wallet = {
+    id: number;
+    value: number;   
+}
+
+export type User = {
+    id: number;
+    name: string;
+}
